04989a49d8074f41cbe844091ce9defeb35abfda
Camilo Hernandez <camehernandezcas@unal.edu.co>
2023-08-05 18:50:36 -0500

    fix reset password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="{{\Illuminate\Support\Facades\Blade::check('feature', 'dark_theme', false) ? "floating-card bg-semi-transparent" : "box-shadow"}}">
                 <div class="card-header">{{ __('Reset Password') }}</div>
 
                 <div class="card-body">
